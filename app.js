a22ccee6163ee2aa57d9e56fbb3bac6e049f880f
cisaa <ci.saavedras@gmail.com>
2022-08-16 15:04:11 -0400

    Implement copy text function

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const result_textarea = document.querySelector('.result_message_text');
 const result_message = document.querySelector('.result_message');
 const wrap_result = document.querySelector('.wrap_result_message');
-const copy_button = document.querySelector('.result_message_button');
 const userTextarea = document.querySelector('.input_user');
 
 const encript_button = document.querySelector('.encriptar_btn');
 const desencript_button = document.querySelector('.desencriptar_btn');
+const copy_button = document.querySelector('.copy_button');
 
 //Event Listener for Buttons
 encript_button.addEventListener('click', encriptButton);
 desencript_button.addEventListener('click', descripButton);
+copy_button.addEventListener('click', copyText);
 
-
+// Functions for encryption and decrypt
 function encriptButton(event) {
       // The preventDefault() method cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur.
       event.preventDefault()
@@ -24,8 +25,6 @@ function encriptButton(event) {
       newText = newText.replace(/a/g, 'ai');
       newText = newText.replace(/o/g, 'ober');
       newText = newText.replace(/u/g, 'ufat');
-      console.log(`newText: ${newText}`);
-
       //Visible and hidden content
       hiddenContent(wrap_result);
       result_textarea.innerText = newText;
@@ -33,11 +32,8 @@ function encriptButton(event) {
       visibleContent(copy_button);
       
 }
-
 function descripButton(event){
-    event.preventDefault()
-    console.log('desencriptando mensaje');
-    
+    event.preventDefault()  
     // let result = result_textarea.value;
     let result = userTextarea.value;
     let newText = result.replace(/enter/g, 'e');
@@ -45,34 +41,24 @@ function descripButton(event){
     newText = newText.replace(/ai/g, 'a');
     newText = newText.replace(/ober/g, 'o');
     newText = newText.replace(/ufat/g, 'u');
-    console.log(`result: ${newText}`);
-
     //Visible and hidden content
     hiddenContent(wrap_result);
     result_textarea.innerText = newText;
     visibleContent(result_textarea);
     visibleContent(copy_button);
+} 
+function copyText(){
+    let data_user = result_textarea.value;
+    const cb = navigator.clipboard;
+    cb.writeText(data_user)
 }
-
-
-// Hidden and visible content display functions
+//  Functions for Hidden and visible content display 
 function hiddenContent(content){
     return content.style.display = 'none';
 }
 function visibleContent(content){
     return content.style.display = 'block';
 }
-
-// TODO
-
-// Las "llaves" de encriptación que utilizaremos son las siguientes:
-
-// `La letra "e" es convertida para "enter"`
-// `La letra "i" es convertida para "imes"`
-// `La letra "a" es convertida para "ai"`
-// `La letra "o" es convertida para "ober"`
-// `La letra "u" es convertida para "ufat"`
-
 
 // **Requisitos:**
 // - Debe funcionar solo con letras minúsculas
